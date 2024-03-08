30bd73169fe8e0862c00b7f724ffe84898ddc696
ABHISHEK DAS <abhishekdas696legit@gmail.com>
2024-03-08 15:52:17 +0530

    Remove unused imports from page component

---
+++ src/app/(public)/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+interface Props {}
+
+export const RegisterPage: React.FC<Props> = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
-import { HeroSection } from "./(public)/landing/_components/hero/HeroSection";
-import { CategorySection } from "./(public)/landing/_components/category/CategorySection";
-import { WtsSection } from "./(public)/landing/_components/where-to-start/WtsSection";
-import { ExploreExperience } from "./(public)/landing/_components/explore-experience/ExploreExperience";
-import { InstructorSection } from "./(public)/landing/_components/instructors/InstructorSection";
-import { UnlockAccessSection } from "./(public)/landing/_components/unlock/UnlockSection";
-import { TestimonialSection } from "./(public)/landing/_components/testimonials/TestimonialSection";
-import { QnaSection } from "./(public)/landing/_components/qna/QnaSection";
 import LandingPage from "./(public)/landing/page";
 
 export default function Page() {
-  // useEffect(() => {
-  //   router.push("/landing");
-  // }, [router]);
-
   return (
     <div>
       <LandingPage />
